71f8466b8c1bdc4c6328aa0910f4530aa0e7ed0a
Duy Bao <nguyenduybaodt1821@gmail.com>
2025-07-10 00:53:31 +0700

    update link

--- frontend/TaskManager/src/utils/socket.js
+++ frontend/TaskManager/src/utils/socket.js
@@ -1,6 +1,6 @@
 // src/utils/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000"); // Địa chỉ backend
+const socket = io("https://manage-task-serve.vercel.app"); // Địa chỉ backend
 
 export default socket;
